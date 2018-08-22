1a5b2cee22d53b55483f40e1ed20e348dab4f53b
Nnavi <nnavi@students.softuni.bg>
2018-08-22 15:28:38 +0300

    Add project files.

---
+++ blankReceipt/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace blankReceipt
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+      {
+        void PrintReceiptHeader()
+        {
+                  Console.WriteLine("CASH RECEIPT");
+                  Console.WriteLine("------------------------------");
+        }
+        void PrintReceiptBody()
+        {
+            Console.WriteLine("Charged to___________________");
+            Console.WriteLine("Received by__________________");
+            Console.WriteLine("------------------------------------");
+        }
+        void PrintReceiptFooter()
+        {
+            Console.WriteLine("\u00A9 SoftUni");
+        }
+        void PrintReceipt()
+        {
+        PrintReceiptHeader();
+        PrintReceiptBody();
+        PrintReceiptFooter();
+        
+        }
+            
+            
+        PrintReceipt();
+            {
+             }
+       }
+    }
+}
