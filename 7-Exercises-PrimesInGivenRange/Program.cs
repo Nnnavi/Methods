27bf0a77ac415035c854d5aa4ad15e5c152beb33
Ivann Ivann <caliban@mail.bg>
2018-08-25 17:12:08 +0300

    exercises 7,8

---
+++ 7-Exercises-PrimesInGivenRange/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6_Exercises_PrimeChecker
+{
+    class Program
+    {
+        static void FindPrimesInRange(int startNum, int endNum)
+        {
+           
+           
+            var primes = "";
+
+            for (int i = startNum; i <= endNum; i++)
+            {
+                if (i == 0 || i == 1)
+                {
+                    i = 2;
+                }
+                bool isPrime = true;
+                for (int j = 2; j <=Math.Sqrt(i); j++)
+                {
+                                      
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primes = primes + i + ", ";
+                }
+            }
+            primes = primes.Substring(0, primes.Length - 2);
+            Console.WriteLine(primes);
+
+            
+        }
+        static void Main(string[] args)
+        {
+           
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+            FindPrimesInRange(startNum,endNum);
+
+        }
+    }
+}
---
+++ 8-Exercises-CenterPoint/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _8_Exercises_CenterPoint
+{
+    class Program
+    {
+        static void CenterPoint(double x1, double y1, double x2, double y2)
+        {
+           
+            if (Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2)) < Math.Sqrt(Math.Pow(x2, 2) + Math.Pow(y2, 2)))
+            {
+                Console.WriteLine("({0}, {1})", x1, y1);
+            }
+            else
+            {
+                Console.WriteLine("({0}, {1})", x2, y2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+           
+            CenterPoint(x1, y1, x2, y2);
+        }
+    }
+}
